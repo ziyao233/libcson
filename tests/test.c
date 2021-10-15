960d060f251f3d9bde94107c2e664e9011afacde
st127 <tswuyin_st127@163.com>
2021-10-15 20:28:35 +0800

    test: Add modern test :)

--- tests/test.c
+++ tests/test.c
@@ -1,11 +1,12 @@
 /*
 	cson
 	File:/tests/test.c
-	Date:2021.09.21
+	Date:2021.10.15
 	By MIT License.
 	Copyright (c) 2021 Suote127.All rights reserved.
 */
 
+#include<assert.h>
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -27,11 +28,23 @@ void print_value(CSON_Context *ctx,const CSON_Value *value)
 	return;
 }
 
-int main(void)
+int main(int argc,const char *argv[])
 {
+	if (argc != 2) {
+		fputs("Configure file path is required\n",stderr);
+		return -1;
+	}
+
+	FILE *conf = fopen(argv[1],"r");
+	assert(conf);
+	fseek(conf,0,SEEK_END);
+	long int size = ftell(conf);
+	rewind(conf);
+
 	char temp[1024] = {0};
 	char *err = NULL;
-	scanf("%s",temp);
+	fread(temp,size,1,conf);
+	fclose(conf);
 	if (cson_parse(temp,print_value,&err,NULL))
 		puts(err);
 	return 0;
